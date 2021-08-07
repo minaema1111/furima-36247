77a3d398873ce791c35352971571efc61bc17da8
minaema1111 <mina.ema.lol@gmail.com>
2021-08-07 14:30:31 +0900

    nicknameテストコード

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper' #pending "add some examples to (or delete) #{__FILE__}"
+
+RSpec.describe User, type: :model do
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000')
+      user.valid?
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
+    end
+    it 'emailが空では登録できない' do
+
+    end
+end
+end
