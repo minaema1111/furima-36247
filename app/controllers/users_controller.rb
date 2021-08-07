52cd6b8b726f6a3bf7a163de97c1477af9434254
minaema1111 <mina.ema.lol@gmail.com>
2021-08-07 11:17:55 +0900

    ログイン、エラーメッセージ表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.all
   end
+
+  def new
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :last_name_kana, presence: true
-         validates :first_name_kana, presence: true
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+         validates :last_name
+         validates :first_name
+         validates :last_name_kana
+         validates :first_name_kana
+         end
          validates :birthday, presence: true
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+         validates_format_of :password, with: PASSWORD_REGEX
 end
