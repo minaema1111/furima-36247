8a2aba3a1787b11ccff1499bfe93cdc1421834d5
minaema1111 <mina.ema.lol@gmail.com>
2021-08-17 12:56:02 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :update]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :ensure_correct_user, only:[:edit]
   
   def index
