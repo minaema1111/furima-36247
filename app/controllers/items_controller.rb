e7cd5c1cac28f7c42cfe89f9c65f7075662bfd63
minaema1111 <mina.ema.lol@gmail.com>
2021-08-17 11:36:23 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :ensure_correct_user, only:[:edit]
-
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.order("created_at DESC")
     end
@@ -21,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path 
     else
@@ -49,4 +46,8 @@ def ensure_correct_user
    unless @item.user == current_user
    redirect_to new_user_session_path
  end
+
+def set_item
+  @item = Item.find(params[:id])
+end
 end
